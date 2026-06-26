c063988b173b2a1c8b2dae509be0361c17a78006
damu72 <damu3910@bht-berlin.de>
2026-06-26 17:41:48 +0200

    Test zum Artikel hinzufügen

--- ShoppingCart/test/ShoppingCartTest.java
+++ ShoppingCart/test/ShoppingCartTest.java
@@ -7,4 +7,12 @@ class ShoppingCartTest {
         ShoppingCart cart = new ShoppingCart();
         assertEquals(0.0, cart.getTotal(), 0.001);
     }
+
+    @Test
+    void testAddItemIncreasesTotal() {
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItem("Apple", 2.50);
+        assertEquals(2.50, cart.getTotal(), 0.001);
 }
+
+
