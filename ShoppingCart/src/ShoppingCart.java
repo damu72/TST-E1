63c4085df52c8d93164ae2ce639ffca9ca13d4ae
damu72 <damu3910@bht-berlin.de>
2026-06-26 17:46:30 +0200

    Datenstruktur und Refactoring

--- ShoppingCart/src/ShoppingCart.java
+++ ShoppingCart/src/ShoppingCart.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+record CartItem(String name, double price) {}
+
 class ShoppingCart {
+    private final List<CartItem> items = new ArrayList<>();
+
+    public void addItem(String name, double price) {
+        items.add(new CartItem(name, price));
+        
+        System.out.println("Liste sieht so aus: " + items);
+    }
+
     public double getTotal() {
+        if (!items.isEmpty()) {
+            return items.get(0).price(); // Fake it till you make it
+        }
         return 0.0;
     }
 }
