4f0a5c6c68011dea0dec25598f0d3949e4e63dfb
damu72 <damu3910@bht-berlin.de>
2026-06-27 14:29:27 +0200

    Test - Artikel zählen

--- ShoppingCart/test/ShoppingCartTest.java
+++ ShoppingCart/test/ShoppingCartTest.java
@@ -22,6 +22,16 @@ class ShoppingCartTest {
         cart.addItem("Banana", 3.50);
         assertEquals(5.50, cart.getTotal(), 0.001);
     }
+
+    @Test
+    void testGetQuantityOfItem() {
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItem("Apple", 1.00);
+        cart.addItem("Apple", 1.00);
+        cart.addItem("Banana", 3.00);
+        
+        assertEquals(2, cart.getQuantity("Apple"));
+    }
 }
 
 
